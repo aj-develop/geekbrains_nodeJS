cb9fcf9e3608ce8e39d80817b39ad0a578194b8b
Alexander Jampolski <aj@Alexanders-MBP.fritz.box>
2021-03-17 17:36:49 +0100

    added 1.3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const moment = require('moment');
+const beep = require("beepbeep");
+const colors = require('colors');
+
 const date = '2018-11-23';
 
 if (moment(date).isValid()) {
-    console.log(moment(date).fromNow());
-    console.log(moment(date).add(10, 'years').format('YYYY-MM-DD'));
+    beep(2, 1000);
+    dateFromNow = moment(date).fromNow();
+    console.log(dateFromNow.green);
+    dateAdd10Years = moment(date).add(10, 'years').format('DD.MM.YYYY');
+    console.log(dateAdd10Years.rainbow);
 }
